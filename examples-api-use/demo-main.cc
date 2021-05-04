25274ab775f9c38c0c79b6b04d4bdcd174537d0c
Dan Choi <ygdanchoi@gmail.com>
2021-05-04 00:20:54 -0400

    aggressive blue

--- examples-api-use/demo-main.cc
+++ examples-api-use/demo-main.cc
@@ -540,7 +540,7 @@ public:
 
       for (int x=0; x<width_; ++x) {
         for (int y=0; y<height_; ++y) {
-          canvas()->SetPixel(x, y, max(r_ - values_[x][y] * 32, 0), max(g_ - values_[x][y] * 48, 0), max(b_ - values_[x][y] * 16, 0));  
+          canvas()->SetPixel(x, y, max(r_ - values_[x][y] * 32, 0), max(g_ - values_[x][y] * 64, 0), max(b_ - values_[x][y] * 8, 0));  
         }
       }
       usleep(delay_ms_ * 1000); // ms

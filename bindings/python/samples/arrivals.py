63dd1801d43c9a6fbf6b7020e258ff9ee231cabc
Dan Choi <ygdanchoi@gmail.com>
2021-08-22 23:22:13 -0400

    duh

--- bindings/python/samples/arrivals.py
+++ bindings/python/samples/arrivals.py
@@ -121,7 +121,7 @@ class Arrivals(SampleBase):
     def __init__(self, *args, **kwargs):
         super(Arrivals, self).__init__(*args, **kwargs)
     
-    async step(self):
+    def async step(self):
         offscreen_canvas = self.matrix.CreateFrameCanvas()
         font = graphics.Font()
         font.LoadFont("../../../fonts/5x7.bdf")

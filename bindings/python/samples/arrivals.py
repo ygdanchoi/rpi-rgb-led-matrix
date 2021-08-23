763e38945adf608a3d02638779b76762026d5e0a
Dan Choi <ygdanchoi@gmail.com>
2021-08-22 21:11:10 -0400

    try accessing lines

--- bindings/python/samples/arrivals.py
+++ bindings/python/samples/arrivals.py
@@ -113,7 +113,6 @@ async def main():
 class Arrivals(SampleBase):
     def __init__(self, *args, **kwargs):
         super(Arrivals, self).__init__(*args, **kwargs)
-        self.parser.add_argument("-t", "--text", help="The text to scroll on the RGB LED panel", default="Hello world!")
 
     def run(self):
         offscreen_canvas = self.matrix.CreateFrameCanvas()
@@ -121,7 +120,7 @@ class Arrivals(SampleBase):
         font.LoadFont("../../../fonts/7x13.bdf")
         textColor = graphics.Color(255, 255, 0)
         pos = offscreen_canvas.width
-        my_text = self.args.text
+        my_text = ' | '.join(lines)
 
         while True:
             offscreen_canvas.Clear()
@@ -139,5 +138,5 @@ if __name__ == "__main__":
     arrivals = Arrivals()
     if (not arrivals.process()):
         arrivals.print_help()
-        
+
 asyncio.run(main())

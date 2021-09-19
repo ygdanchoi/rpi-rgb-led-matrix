0b68c2118d03ae9fde50746df6badee658b272ca
Dan Choi <ygdanchoi@gmail.com>
2021-09-18 23:18:27 -0400

    break into rudimentary classes

--- bindings/python/arrivals/arrivals.py
+++ bindings/python/arrivals/arrivals.py
@@ -16,144 +16,108 @@ import traceback
 
 Trip = collections.namedtuple('Trip', ['trip_headsign', 'route_id', 'direction_id'])
 TransitLine = collections.namedtuple('TransitLine', ['route_id', 'trip_id', 'trip_headsign', 'eta', 'color'])
-
-class TransitLineNew():
-    def __init__(self, name, direction, description, etas, color):
-        self.name = name
-        self.direction = direction
-        self.description = description
-        self.etas = etas
-        self.color = color
-
-    def key(self):
-        return f'{self.name}-{self.direction}'
-
 cached_rows = []
-trips = {}
-colors = {}
 
-with open('./gtfs/mta-subway/google_transit/trips.txt') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    should_skip_header_row = True
-    for row in csv_reader:
-        if should_skip_header_row:
-            should_skip_header_row = False
-            continue
-        route_id = row[0]
-        trip_id = '_'.join(row[2].split('_')[1:])[:-3]
-        trip_headsign = row[3]
-        direction_id = row[4]
+# class TransitLineNew:
+#     def __init__(self, name, direction, description, etas, color):
+#         self.name = name
+#         self.direction = direction
+#         self.description = description
+#         self.etas = etas
+#         self.color = color
 
-        trips[trip_id] = Trip(
-            trip_headsign=trip_headsign,
-            route_id=route_id,
-            direction_id=direction_id
+#     def key(self):
+#         return f'{self.name}-{self.direction}'
+
+class GtfsService:
+    def get_gtfs_eta(self, trip_update, stop_id, current_time):
+        arrival_time = next(
+            (stop_time_update.arrival.time for stop_time_update in trip_update.stop_time_update if stop_time_update.stop_id == stop_id),
+            None
         )
 
-with open('./gtfs/mta-subway/google_transit/routes.txt') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    should_skip_header_row = True
-    for row in csv_reader:
-        if should_skip_header_row:
-            should_skip_header_row = False
-            continue
-        route_id = row[0]
-        route_color = row[7]
-
-        if len(route_color) == 6:
-            colors[route_id] = [
-                int(route_color[0:2], 16),
-                int(route_color[2:4], 16),
-                int(route_color[4:6], 16)
-            ]
+        if arrival_time is None:
+            return -1
         else:
-            colors[route_id] = [255, 255, 255]
-        
-with open('./gtfs/nyc-ferry/google_transit/trips.txt') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    should_skip_header_row = True
-    for row in csv_reader:
-        if should_skip_header_row:
-            should_skip_header_row = False
-            continue
-        route_id = row[0]
-        trip_id = row[2]
-        trip_headsign = row[3]
-        direction_id = row[5]
+            return int(round((arrival_time - current_time) / 60, 0))
 
-        trips[trip_id] = Trip(
-            trip_headsign=trip_headsign,
-            route_id=route_id,
-            direction_id=direction_id
-        )
 
-with open('./gtfs/nyc-ferry/google_transit/routes.txt') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    should_skip_header_row = True
-    for row in csv_reader:
-        if should_skip_header_row:
-            should_skip_header_row = False
-            continue
-        route_id = row[0]
-        route_color = row[5]
+class MtaSubwayService(GtfsService):
+    def __init__(self):
+        self.trips = {}
+        self.colors = {}
 
-        if len(route_color) == 6:
-            colors[route_id] = [
-                int(route_color[0:2], 16),
-                int(route_color[2:4], 16),
-                int(route_color[4:6], 16)
-            ]
-        else:
-            colors[route_id] = [255, 255, 255]
+        with open('./gtfs/mta-subway/google_transit/trips.txt') as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            should_skip_header_row = True
+            for row in csv_reader:
+                if should_skip_header_row:
+                    should_skip_header_row = False
+                    continue
+                route_id = row[0]
+                trip_id = '_'.join(row[2].split('_')[1:])[:-3]
+                trip_headsign = row[3]
+                direction_id = row[4]
 
-class FetchArrivals(threading.Thread):
-    def run(self):
-        while True:
-            self.fetch_arrivals()
-            time.sleep(30)
+                self.trips[trip_id] = Trip(
+                    trip_headsign=trip_headsign,
+                    route_id=route_id,
+                    direction_id=direction_id
+                )
+
+        with open('./gtfs/mta-subway/google_transit/routes.txt') as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            should_skip_header_row = True
+            for row in csv_reader:
+                if should_skip_header_row:
+                    should_skip_header_row = False
+                    continue
+                route_id = row[0]
+                route_color = row[7]
+
+                if len(route_color) == 6:
+                    self.colors[route_id] = [
+                        int(route_color[0:2], 16),
+                        int(route_color[2:4], 16),
+                        int(route_color[4:6], 16)
+                    ]
+                else:
+                    self.colors[route_id] = [255, 255, 255]
     
-    def fetch_arrivals(self):
-        arrivals = collections.OrderedDict()
-        current_time = time.time()
+    def put_gtfs_arrivals(self, url, stop_id, direction, arrivals, current_time):
+        response = requests.get(url, headers={
+            'x-api-key': secrets.real_time_access_key
+        })
+        feed = gtfs_realtime_pb2.FeedMessage()
+        feed.ParseFromString(response.content)
+        entities = feed.entity
 
-        try:
-            self.put_gtfs_arrivals(
-                'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs', #1234567
-                '626S', # 86 St
-                '..S',
-                arrivals,
-                current_time
-            )
-            self.put_gtfs_arrivals(
-                'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-nqrw',
-                'Q05S', # 96 St,
-                '..S',
-                arrivals,
-                current_time
-            )
-            self.put_siri_arrivals(
-                '404947',
-                arrivals,
-                current_time
-            )
-            self.put_gtfs_arrivals_ferry(
-                '113', # 86 St
-                '0', # southbound
-                arrivals,
-                current_time
-            )
-        except Exception as error:
-            arrivals['ERR!'] = [TransitLine(
-                route_id='ERR!',
-                trip_id='ERR!',
-                trip_headsign=str(error),
-                eta=0,
-                color=[255, 0, 0]
-            )]
-            traceback.print_exc()
+        for entity in filter(lambda entity: entity.HasField('trip_update'), entities):
+            trip_update = entity.trip_update
+            eta = self.get_gtfs_eta(trip_update, stop_id, current_time)
+            trip_id = trip_update.trip.trip_id
 
-        cached_rows.clear()
-        cached_rows.extend(arrivals.values())
+            if eta > 0 and direction in trip_id:
+                route_id = trip_update.trip.route_id
+                if route_id not in arrivals:
+                    arrivals[route_id] = []
+
+                if (trip_id not in self.trips):
+                    trip_keys = list(sorted(filter(lambda key: '_' in key and key.split('_')[1] in trip_id.split('_')[1], trips.keys())))
+                    i = bisect.bisect_left(trip_keys, trip_id)
+                    trip_id = trip_keys[min(i, len(trip_keys) - 1)]
+
+                arrivals[route_id].append(TransitLine(
+                    route_id=route_id,
+                    trip_id=trip_id,
+                    trip_headsign=self.trips[trip_id].trip_headsign,
+                    eta=eta,
+                    color=self.colors[route_id]
+                ))
+
+class MtaBusService:
+    def __init__(self):
+        self.trips = {}
     
     def put_siri_arrivals(self, stop_id, arrivals, current_time):
         response = requests.get("https://bustime.mta.info/api/siri/stop-monitoring.json", params={
@@ -184,36 +148,47 @@ class FetchArrivals(threading.Thread):
                     color=[0, 57, 166]
                 ))
 
-    def put_gtfs_arrivals(self, url, stop_id, direction, arrivals, current_time):
-        response = requests.get(url, headers={
-            'x-api-key': secrets.real_time_access_key
-        })
-        feed = gtfs_realtime_pb2.FeedMessage()
-        feed.ParseFromString(response.content)
-        entities = feed.entity
+class NycFerryService(GtfsService):
+    def __init__(self):
+        self.trips = {}
+        self.colors = {}
 
-        for entity in filter(lambda entity: entity.HasField('trip_update'), entities):
-            trip_update = entity.trip_update
-            eta = self.get_gtfs_eta(trip_update, stop_id, current_time)
-            trip_id = trip_update.trip.trip_id
+        with open('./gtfs/nyc-ferry/google_transit/trips.txt') as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            should_skip_header_row = True
+            for row in csv_reader:
+                if should_skip_header_row:
+                    should_skip_header_row = False
+                    continue
+                route_id = row[0]
+                trip_id = row[2]
+                trip_headsign = row[3]
+                direction_id = row[5]
 
-            if eta > 0 and direction in trip_id:
-                route_id = trip_update.trip.route_id
-                if route_id not in arrivals:
-                    arrivals[route_id] = []
-
-                if (trip_id not in trips):
-                    trip_keys = list(sorted(filter(lambda key: '_' in key and key.split('_')[1] in trip_id.split('_')[1], trips.keys())))
-                    i = bisect.bisect_left(trip_keys, trip_id)
-                    trip_id = trip_keys[min(i, len(trip_keys) - 1)]
-
-                arrivals[route_id].append(TransitLine(
+                self.trips[trip_id] = Trip(
+                    trip_headsign=trip_headsign,
                     route_id=route_id,
-                    trip_id=trip_id,
-                    trip_headsign=trips[trip_id].trip_headsign,
-                    eta=eta,
-                    color=colors[route_id]
-                ))
+                    direction_id=direction_id
+                )
+
+        with open('./gtfs/nyc-ferry/google_transit/routes.txt') as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            should_skip_header_row = True
+            for row in csv_reader:
+                if should_skip_header_row:
+                    should_skip_header_row = False
+                    continue
+                route_id = row[0]
+                route_color = row[5]
+
+                if len(route_color) == 6:
+                    self.colors[route_id] = [
+                        int(route_color[0:2], 16),
+                        int(route_color[2:4], 16),
+                        int(route_color[4:6], 16)
+                    ]
+                else:
+                    self.colors[route_id] = [255, 255, 255]
 
     def put_gtfs_arrivals_ferry(self, stop_id, direction_id, arrivals, current_time):
         response = requests.get('http://nycferry.connexionz.net/rtt/public/utility/gtfsrealtime.aspx/tripupdate')
@@ -226,28 +201,71 @@ class FetchArrivals(threading.Thread):
             eta = self.get_gtfs_eta(trip_update, stop_id, current_time)
             trip_id = trip_update.trip.trip_id
 
-            if eta > 0 and trips[trip_id].direction_id == direction_id:
-                route_id = trips[trip_id].route_id
+            if eta > 0 and self.trips[trip_id].direction_id == direction_id:
+                route_id = self.trips[trip_id].route_id
                 if route_id not in arrivals:
                     arrivals[route_id] = []
                 arrivals[route_id].append(TransitLine(
                     route_id=route_id,
                     trip_id=trip_id,
-                    trip_headsign=trips[trip_id].trip_headsign,
+                    trip_headsign=self.trips[trip_id].trip_headsign,
                     eta=eta,
-                    color=colors[route_id]
+                    color=self.colors[route_id]
                 ))
 
-    def get_gtfs_eta(self, trip_update, stop_id, current_time):
-        arrival_time = next(
-            (stop_time_update.arrival.time for stop_time_update in trip_update.stop_time_update if stop_time_update.stop_id == stop_id),
-            None
-        )
+class FetchArrivals(threading.Thread):
+    def __init__(self):
+        self.mta_subway_service = MtaSubwayService()
+        self.mta_bus_service = MtaBusService()
+        self.nyc_ferry_service = NycFerryService()
 
-        if arrival_time is None:
-            return -1
-        else:
-            return int(round((arrival_time - current_time) / 60, 0))
+    def run(self):
+        while True:
+            self.fetch_arrivals()
+            time.sleep(30)
+    
+    def fetch_arrivals(self):
+        arrivals = collections.OrderedDict()
+        current_time = time.time()
+
+        try:
+            self.mta_subway_service.put_gtfs_arrivals(
+                'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs', #1234567
+                '626S', # 86 St
+                '..S',
+                arrivals,
+                current_time
+            )
+            self.mta_subway_service.put_gtfs_arrivals(
+                'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-nqrw',
+                'Q05S', # 96 St,
+                '..S',
+                arrivals,
+                current_time
+            )
+            self.mta_bus_service.put_siri_arrivals(
+                '404947',
+                arrivals,
+                current_time
+            )
+            self.nyc_ferry_service.put_gtfs_arrivals_ferry(
+                '113', # 86 St
+                '0', # southbound
+                arrivals,
+                current_time
+            )
+        except Exception as error:
+            arrivals['ERR!'] = [TransitLine(
+                route_id='ERR!',
+                trip_id='ERR!',
+                trip_headsign=str(error),
+                eta=0,
+                color=[255, 0, 0]
+            )]
+            traceback.print_exc()
+
+        cached_rows.clear()
+        cached_rows.extend(arrivals.values())
 
 class RgbMatrixView(SampleBase):
     def __init__(self, *args, **kwargs):

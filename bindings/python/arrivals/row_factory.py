47a566ea0d776bec7dbe0d54b70e5d703a153f23
Dan Choi <ygdanchoi@gmail.com>
2021-09-19 21:43:59 -0400

    space

--- bindings/python/arrivals/row_factory.py
+++ bindings/python/arrivals/row_factory.py
@@ -24,6 +24,7 @@ class RowFactory:
                 else:
                     break
             text += 'm'
+            
             rows.append(Row(
                 text=text,
                 color=transit_line.color

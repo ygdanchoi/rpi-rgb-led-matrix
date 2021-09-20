abc3dc2e0ecdf69c42e6414e8de076aa61f7ef46
Dan Choi <ygdanchoi@gmail.com>
2021-09-19 22:50:02 -0400

    Revert "maybe proper mvvm"
    This reverts commit 91b90db6976bee3a09c8a4bd4f111ab3c7241866.

--- bindings/python/arrivals/transit_feed_view.py
+++ bindings/python/arrivals/transit_feed_view.py
@@ -10,7 +10,6 @@ class TransitFeedViewModel():
     def __init__(self, transit_service, row_factory):
         self.transit_service = transit_service
         self.row_factory = row_factory
-        self.observers = []
         
         self.vertical_offset = 0
         self.row_height = 7
@@ -21,10 +20,6 @@ class TransitFeedViewModel():
 
         threading.Thread(target=self.update_transit_lines).start()
         threading.Thread(target=self.update_rows).start()
-        threading.Thread(target=self.update_vertical_offset_and_notify_observers).start()
-    
-    def register_observer(self, observer):
-        self.observers.append(observer)
 
     def update_transit_lines(self):
         while True:
@@ -36,14 +31,10 @@ class TransitFeedViewModel():
             self.rows = self.row_factory.create_rows(self.transit_lines)
             time.sleep(1)
     
-    def update_vertical_offset_and_notify_observers(self):
-        while True:
-            self.vertical_offset += 1
-            if len(self.transit_lines) < self.max_rows or self.vertical_offset >= self.row_height * len(self.transit_lines):
-                self.vertical_offset = 0
-            for observer in self.observers:
-                observer.notify()
-            time.sleep(0.1)
+    def update_vertical_offset(self):
+        self.vertical_offset += 1
+        if len(self.transit_lines) < self.max_rows or self.vertical_offset >= self.row_height * len(self.transit_lines):
+            self.vertical_offset = 0
     
     def is_light_mode(self):
         hh = datetime.now().hour
@@ -57,42 +48,41 @@ class TransitFeedView(SampleBase):
             transit_service=kwargs['transit_service'],
             row_factory=kwargs['row_factory']
         )
-        self.viewmodel.register_observer(self)
 
     def run(self):
-        self.offscreen_canvas = self.matrix.CreateFrameCanvas()
-        self.font = graphics.Font()
-        self.font.LoadFont("../../../fonts/tom-thumb.bdf")
-        self.dark_mode_color = graphics.Color(47, 0, 0)
-    
-    def notify(self):
-        self.offscreen_canvas.Clear()
-        rows = self.viewmodel.rows
-        is_light_mode = self.viewmodel.is_light_mode()
-
-        for i, row in enumerate(rows if len(rows) < self.viewmodel.max_rows else rows + rows):                
-            graphics.DrawText(
-                self.offscreen_canvas,
-                self.font,
-                1,
-                7 + i * self.viewmodel.row_height - self.viewmodel.vertical_offset,
-                graphics.Color(*row.color) if is_light_mode else self.dark_mode_color,
-                row.text
-            )
-
-            for y in range(self.offscreen_canvas.height - self.viewmodel.row_height, self.offscreen_canvas.height):
-                for x in range(0, self.offscreen_canvas.width):
-                    self.offscreen_canvas.SetPixel(x, y, 0, 0, 0)
-            
-            graphics.DrawText(
-                self.offscreen_canvas,
-                self.font,
-                1,
-                self.offscreen_canvas.height - 1,
-                graphics.Color(255, 255, 255) if is_light_mode else self.dark_mode_color,
-                datetime.now().strftime('%a, %b %d, %Y  %-I:%M:%S %p')
-            )
+        offscreen_canvas = self.matrix.CreateFrameCanvas()
+        font = graphics.Font()
+        font.LoadFont("../../../fonts/tom-thumb.bdf")
+        dark_mode_color = graphics.Color(47, 0, 0)
         
-        self.offscreen_canvas = self.matrix.SwapOnVSync(self.offscreen_canvas)
-        self.viewmodel.update_vertical_offset()
-        time.sleep(0.1)
+        while True:
+            offscreen_canvas.Clear()
+            rows = self.viewmodel.rows
+            is_light_mode = self.viewmodel.is_light_mode()
+
+            for i, row in enumerate(rows if len(rows) < self.viewmodel.max_rows else rows + rows):                
+                graphics.DrawText(
+                    offscreen_canvas,
+                    font,
+                    1,
+                    7 + i * self.viewmodel.row_height - self.viewmodel.vertical_offset,
+                    graphics.Color(*row.color) if is_light_mode else dark_mode_color,
+                    row.text
+                )
+
+                for y in range(offscreen_canvas.height - self.viewmodel.row_height, offscreen_canvas.height):
+                    for x in range(0, offscreen_canvas.width):
+                        offscreen_canvas.SetPixel(x, y, 0, 0, 0)
+                
+                graphics.DrawText(
+                    offscreen_canvas,
+                    font,
+                    1,
+                    offscreen_canvas.height - 1,
+                    graphics.Color(255, 255, 255) if is_light_mode else dark_mode_color,
+                    datetime.now().strftime('%a, %b %d, %Y  %-I:%M:%S %p')
+                )
+            
+            offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
+            self.viewmodel.update_vertical_offset()
+            time.sleep(0.1)

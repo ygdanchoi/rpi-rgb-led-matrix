a97c7afbb551dd9373d33ff2a4ccc6809cfad8f6
Dan Choi <ygdanchoi@gmail.com>
2021-09-21 18:58:38 -0400

    i cant believe that shit worked

--- bindings/python/arrivals/transit_feed_view.py
+++ bindings/python/arrivals/transit_feed_view.py
@@ -97,7 +97,7 @@ class TransitFeedView(SampleBase):
                             min(1, 1 + y - 4 - 3 * self.viewmodel.row_height),
                             y,
                             light_mode_color if is_light_mode else dark_mode_color,
-                            '123456789'[:(4 + max(0, 6 - y // self.viewmodel.row_width))] # row.name
+                            row.name[:(4 + max(0, 6 - y // self.viewmodel.row_width))]
                         )
 
                         for yy in range(y - self.viewmodel.row_height + 2, y + 1):

a961f1837dd50015b7686312d88b7e3d694a5cac
Dan Choi <ygdanchoi@gmail.com>
2021-09-21 18:38:10 -0400

    are u fcking kidding me

--- bindings/python/arrivals/transit_feed_view.py
+++ bindings/python/arrivals/transit_feed_view.py
@@ -121,7 +121,7 @@ class TransitFeedView(SampleBase):
                         graphics.DrawText(
                             offscreen_canvas,
                             font,
-                            min(5, 5 + y - 4 - 3 * self.viewmodel.row_height)1,
+                            min(5, 5 + y - 4 - 3 * self.viewmodel.row_height),
                             y,
                             light_mode_color if is_light_mode else dark_mode_color,
                             row.description

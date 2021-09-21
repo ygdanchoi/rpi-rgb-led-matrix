abe7ae6f362c90e6af02ac931bbb707d3fc70318
Dan Choi <ygdanchoi@gmail.com>
2021-09-21 18:03:39 -0400

    i wonder what will happen

--- bindings/python/arrivals/transit_feed_view.py
+++ bindings/python/arrivals/transit_feed_view.py
@@ -102,7 +102,7 @@ class TransitFeedView(SampleBase):
                         font,
                         1 + 25 * self.viewmodel.row_width,
                         y,
-                        graphics.Color(255, 255, 255) if is_light_mode else dark_mode_color,
+                        light_mode_color if is_light_mode else dark_mode_color,
                         row.etas
                     )
 

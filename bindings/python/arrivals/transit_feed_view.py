bbb0e7ef4a6257f1c5032b7dc5e9d5c2d156c265
Dan Choi <ygdanchoi@gmail.com>
2021-09-21 18:26:42 -0400

    well technically

--- bindings/python/arrivals/transit_feed_view.py
+++ bindings/python/arrivals/transit_feed_view.py
@@ -83,7 +83,7 @@ class TransitFeedView(SampleBase):
 
                     if should_scroll_name:
                         for yy in range(y - self.viewmodel.row_height + 2, y + 1):
-                            for xx in range(5 * self.viewmodel.row_width, offscreen_canvas.width):
+                            for xx in range(4 * self.viewmodel.row_width, offscreen_canvas.width):
                                 offscreen_canvas.SetPixel(xx, yy, 0, 0, 0)
                     
                         graphics.DrawText(

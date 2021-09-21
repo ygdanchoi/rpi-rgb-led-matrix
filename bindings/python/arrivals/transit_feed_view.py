9c3fcaf860735a5385afde4bc9fa6fd73d81f1f2
Dan Choi <ygdanchoi@gmail.com>
2021-09-21 18:17:09 -0400

    is this the right paramter

--- bindings/python/arrivals/transit_feed_view.py
+++ bindings/python/arrivals/transit_feed_view.py
@@ -75,7 +75,7 @@ class TransitFeedView(SampleBase):
                     graphics.DrawText(
                         offscreen_canvas,
                         font,
-                        min(1, 1 + y - 4 * self.viewmodel.row_height) if should_scroll_name else 1,
+                        min(1, 10 + y - 4 * self.viewmodel.row_height) if should_scroll_name else 1,
                         y,
                         light_mode_color if is_light_mode else dark_mode_color,
                         row.name

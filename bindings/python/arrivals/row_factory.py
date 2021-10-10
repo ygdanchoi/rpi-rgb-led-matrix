c84534aeef2439c7355e2618072697f3459555f3
Dan Choi <ygdanchoi@gmail.com>
2021-10-10 13:52:19 -0400

    goddammit lol

--- bindings/python/arrivals/row_factory.py
+++ bindings/python/arrivals/row_factory.py
@@ -18,7 +18,7 @@ class RowFactory:
             
             y = (len(rows) + 1) * cell_height - vertical_offset
             if y < -cell_height:
-                y += len(transit_lines) * cell_height
+                y += len(rows) * cell_height
             
             rows.append(Row(
                 name=transit_line.name,
--- bindings/python/arrivals/transit_feed_view.py
+++ bindings/python/arrivals/transit_feed_view.py
@@ -58,7 +58,7 @@ class TransitFeedViewModel(Subject):
             self.increment_offsets()
             
             last_delta_s = (time.time_ns() - last_ns) / 1_000_000_000
-            s_to_wait = max(0, 0.05 - last_delta_s)
+            s_to_wait = max(0, 0.06 - last_delta_s)
             time.sleep(s_to_wait)
             last_ns = time.time_ns()
 
@@ -80,7 +80,6 @@ class TransitFeedViewModel(Subject):
             update_weather_timer -= 1
             
             time.sleep(1)
-
     
     def increment_offsets(self):
         self.horizontal_offset += 1

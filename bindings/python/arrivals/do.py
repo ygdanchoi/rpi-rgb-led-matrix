6bb7eec4c382966e7f5b349dfc230f943d505031
Dan Choi <ygdanchoi@gmail.com>
2021-10-11 22:08:36 -0400

    better windows rendering

--- bindings/python/arrivals/do.py
+++ bindings/python/arrivals/do.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 import os
 
+from datetime import datetime
+
 from row_factory import RowFactory
 from transit_service import CompositeTransitService as TransitService
 from weather_service import WeatherService
@@ -19,6 +21,15 @@ if __name__ == "__main__":
         ).process()
     else:
         transit_lines = transit_service.get_transit_lines()
-        for row in row_factory.create_rows(transit_lines, 0, 0, 1, 1):
+        rows = row_factory.create_rows(
+            transit_lines=transit_lines,
+            vertical_offset=0,
+            horizontal_offset=0,
+            cell_height=7,
+            cell_width=4
+        )
+        for row in rows:
             print(f'{row.name[:4]:<5}{row.description[:17]:<19}{row.etas}')
-        print(weather_service.get_weather().temperature)
+
+        temperature = weather_service.get_weather().temperature
+        print(f"{datetime.now().strftime('%a, %b %d • %I:%M:%S %p')}{f' • {temperature}' if temperature else ''}")

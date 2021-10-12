df8f3be0f7a1f810871bffef43a14c56be88d63e
Dan Choi <ygdanchoi@gmail.com>
2021-10-12 06:19:18 -0400

    maybe actully here

--- bindings/python/arrivals/row_factory.py
+++ bindings/python/arrivals/row_factory.py
@@ -42,12 +42,12 @@ class RowFactory:
                 dx_name = -self.beveled_zigzag(
                     max_rows * cell_height + 3 - pseudo_y,
                     (len(transit_line.name) - 4) * cell_width,
-                    2 * cell_height
+                    2 * cell_height - 1
                 ) if should_scroll_name else 0,
                 dx_description = -self.beveled_zigzag(
                     max_rows * cell_height + 3 - pseudo_y,
                     (len(transit_line.description) - 17) * cell_width,
-                    2 * cell_height
+                    2 * cell_height - 1
                 ) if should_scroll_description else 0
             ))
 

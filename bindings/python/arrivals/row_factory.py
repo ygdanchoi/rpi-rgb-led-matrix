fb396cb83c46b885822d51ee1dce712dbb3160aa
Dan Choi <ygdanchoi@gmail.com>
2021-10-12 06:12:38 -0400

    negate

--- bindings/python/arrivals/row_factory.py
+++ bindings/python/arrivals/row_factory.py
@@ -39,13 +39,13 @@ class RowFactory:
                 color=transit_line.color,
                 y = y,
                 # TODO: define _/‾\_/‾\ cycle based on horizontal_offset & string lengths
-                dx_name = self.beveled_zigzag(
-                    pseudo_y,
+                dx_name = -self.beveled_zigzag(
+                    -pseudo_y,
                     (len(transit_line.name) - 4) * cell_width - 1,
                     2 * cell_height - 1
                 ) if should_scroll_name else 0,
-                dx_description = self.beveled_zigzag(
-                    pseudo_y,
+                dx_description = -self.beveled_zigzag(
+                    -pseudo_y,
                     (len(transit_line.description) - 17) * cell_width - 1,
                     2 * cell_height - 1
                 ) if should_scroll_description else 0

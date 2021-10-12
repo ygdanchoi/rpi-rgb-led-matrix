f348e5ae1b87cf5b9019fe3f24afa1942016ecbf
Dan Choi <ygdanchoi@gmail.com>
2021-10-12 06:49:33 -0400

    closer

--- bindings/python/arrivals/transit_feed_view.py
+++ bindings/python/arrivals/transit_feed_view.py
@@ -174,7 +174,7 @@ class TransitFeedView(Observer, SampleBase):
             row,
             1 + row.dx_name,
             # TODO: should calculate index from row.dx_name, not row.y
-            row.name[:(self.viewmodel.idx_desc - 1 + max(0, -(row.dx_name + 1) // self.viewmodel.cell_width))]
+            row.name[:(self.viewmodel.idx_desc - 1 + max(0, -(row.dx_name) // self.viewmodel.cell_width))]
         )
         self.draw_row_mask(row, (self.viewmodel.idx_desc - 1) * self.viewmodel.cell_width, self.viewmodel.idx_desc * self.viewmodel.cell_width)
 

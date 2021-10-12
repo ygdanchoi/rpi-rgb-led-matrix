8b91c15bb9da5296d6002cf4484ba900da37d776
Dan Choi <ygdanchoi@gmail.com>
2021-10-12 06:08:50 -0400

    hmm

--- bindings/python/arrivals/row_factory.py
+++ bindings/python/arrivals/row_factory.py
@@ -40,12 +40,12 @@ class RowFactory:
                 y = y,
                 # TODO: define _/‾\_/‾\ cycle based on horizontal_offset & string lengths
                 dx_name = -self.beveled_zigzag(
-                    horizontal_offset - (i + 1) * cell_height,
+                    horizontal_offset - (i + 0) * cell_height,
                     (len(transit_line.name) - 4) * cell_width - 1,
                     2 * cell_height - 1
                 ) if should_scroll_name else 0,
                 dx_description = -self.beveled_zigzag(
-                    horizontal_offset - (i + 1) * cell_height,
+                    horizontal_offset - (i + 0) * cell_height,
                     (len(transit_line.description) - 17) * cell_width - 1,
                     2 * cell_height - 1
                 ) if should_scroll_description else 0

ebbb3cde16fff431d84ff80bdd5ea2714fb4f428
Dan Choi <ygdanchoi@gmail.com>
2021-10-12 06:24:23 -0400

    try 3

--- bindings/python/arrivals/row_factory.py
+++ bindings/python/arrivals/row_factory.py
@@ -39,12 +39,12 @@ class RowFactory:
                 color=transit_line.color,
                 y = y,
                 dx_name = -self.beveled_zigzag(
-                    max_rows * cell_height + 4 - pseudo_y,
+                    max_rows * cell_height + 3 - pseudo_y,
                     (len(transit_line.name) - 4) * cell_width,
                     2 * cell_height
                 ) if should_scroll_name else 0,
                 dx_description = -self.beveled_zigzag(
-                    max_rows * cell_height + 4 - pseudo_y,
+                    max_rows * cell_height + 3 - pseudo_y,
                     (len(transit_line.description) - 17) * cell_width,
                     2 * cell_height
                 ) if should_scroll_description else 0

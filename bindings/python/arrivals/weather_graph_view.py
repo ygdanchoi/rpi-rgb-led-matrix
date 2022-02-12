6ea9c5df965f808281189ecd3b079375e2c7a3f9
Dan Choi <ygdanchoi@gmail.com>
2022-02-12 13:22:09 -0500

    this has tobe it

--- bindings/python/arrivals/weather_graph_view.py
+++ bindings/python/arrivals/weather_graph_view.py
@@ -120,7 +120,7 @@ class WeatherGraphView(Observer, SampleBase):
         for i, weather_hour in enumerate(self.viewmodel.forecast[0:25]):
             self.offscreen_canvas.SetPixel(
                 i / 24 * 114 + 6,
-                self.offscreen_canvas.height - 10 - 0 * (self.offscreen_canvas.height - 2 * self.viewmodel.cell_height) * (weather_hour.temp - min_temp) / (max_temp - min_temp),
+                self.viewmodel.cell_height + (self.offscreen_canvas.height - 2 * self.viewmodel.cell_height) * (weather_hour.temp - min_temp) / (max_temp - min_temp),
                 255,
                 255,
                 255

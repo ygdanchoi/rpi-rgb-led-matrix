5f5429097da90d5e6f7e960e58aa717d30edbe32
Dan Choi <ygdanchoi@gmail.com>
2022-02-12 14:58:47 -0500

    try list comp

--- bindings/python/arrivals/weather_graph_view.py
+++ bindings/python/arrivals/weather_graph_view.py
@@ -135,10 +135,9 @@ class WeatherGraphView(Observer, SampleBase):
                         self.draw_stripe_pixel(x, yy, point.color)
         for i, point in enumerate(points[2:27:4]):
             label = f"{int(round(point.temp, 0))}°"
-            print([points[i - 1].y, point.y, points[i + 1].y])
             self.draw_text(
                 7 + i * 19 - len(label) * self.viewmodel.cell_width / 2,
-                min(points[i - 1].y, point.y, points[i + 1].y) - 1,
+                min([pt.y - 1 for pt in points[(i - 2):(i + 2)]]),
                 label
             )
 

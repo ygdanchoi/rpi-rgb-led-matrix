c4ac4d019e342391fc7173f4638afe4b4ae4947d
Dan Choi <ygdanchoi@gmail.com>
2022-02-12 19:19:04 -0500

    just half

--- bindings/python/arrivals/weather_graph_view.py
+++ bindings/python/arrivals/weather_graph_view.py
@@ -329,15 +329,15 @@ class WeatherGraphView(Observer, SampleBase):
                     xx,
                     yy,
                     max(
-                        color[0] * 3 // 5 + self.viewmodel.get_gol_safe(yy, xx) * 16,
+                        color[0] // 2 + self.viewmodel.get_gol_safe(yy, xx) * 16,
                         color[0] // stripe_divisor
                     ),
                     max(
-                        color[1] * 3 // 5 + self.viewmodel.get_gol_safe(yy, xx) * 32,
+                        color[1] // 2 + self.viewmodel.get_gol_safe(yy, xx) * 32,
                         color[1] // stripe_divisor
                     ),
                     max(
-                        color[2] * 3 // 5 + self.viewmodel.get_gol_safe(yy, xx) * 4,
+                        color[2] // 2 + self.viewmodel.get_gol_safe(yy, xx) * 4,
                         color[2] // stripe_divisor
                     )
                 )

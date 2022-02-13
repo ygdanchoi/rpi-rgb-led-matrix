4b9d7337df000a237951a4c9b2d859f275fc29a6
Dan Choi <ygdanchoi@gmail.com>
2022-02-13 07:27:03 -0500

    1px less

--- bindings/python/arrivals/weather_graph_view.py
+++ bindings/python/arrivals/weather_graph_view.py
@@ -111,7 +111,7 @@ class WeatherGraphViewModel(Observable):
         elif 500 <= code and code <= 599: # rain
             return [81, 121, 203]
         elif 600 <= code and code <= 699: # snow
-            return [219, 219, 219]
+            return [218, 218, 218]
         elif 700 <= code and code <= 799: # fog
             return [50, 182, 122] # unseen
         elif 800 <= code and code <= 802: # clear

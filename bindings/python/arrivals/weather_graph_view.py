1eb56430ecb377105002373f4619524dd9ff02af
Dan Choi <ygdanchoi@gmail.com>
2022-02-12 20:14:37 -0500

    get color

--- bindings/python/arrivals/weather_graph_view.py
+++ bindings/python/arrivals/weather_graph_view.py
@@ -104,6 +104,30 @@ class WeatherGraphViewModel(Observable):
 
         return points
 
+    # https://www.weatherbit.io/api/codes
+    def get_color(self, weather_hour):
+        code = weather_hour.code
+
+        if 200 <= code and code <= 299: # thunderstorm
+            return [243, 121, 203] # unseen
+        elif 300 <= code and code <= 399: # drizzle
+            return [81, 121, 203]
+        elif 500 <= code and code <= 599: # rain
+            return [81, 121, 203]
+        elif 600 <= code and code <= 699: # snow
+            return [191, 191, 223]
+        elif 700 <= code and code <= 799: # fog
+            return [50, 182, 122] # unseen
+        elif 800 <= code and code <= 802: # clear
+            return [243, 179, 67]
+        elif 803 <= code and code <= 899: # clouds
+            return [139, 145, 158]
+        else:
+            return [
+                random.randint(64, 255),
+                random.randint(64, 255),
+                random.randint(64, 255)
+        ]
     
     def increment_offsets(self):
         self.stripes_offset += 1
@@ -252,31 +276,6 @@ class WeatherGraphView(Observer, SampleBase):
 
         self.offscreen_canvas = self.matrix.SwapOnVSync(self.offscreen_canvas)
     
-    # https://www.weatherbit.io/api/codes
-    def get_color(self, weather_hour):
-        code = weather_hour.code
-
-        if 200 <= code and code <= 299: # thunderstorm
-            return [243, 121, 203] # unseen
-        elif 300 <= code and code <= 399: # drizzle
-            return [81, 121, 203]
-        elif 500 <= code and code <= 599: # rain
-            return [81, 121, 203]
-        elif 600 <= code and code <= 699: # snow
-            return [191, 191, 223]
-        elif 700 <= code and code <= 799: # fog
-            return [50, 182, 122] # unseen
-        elif 800 <= code and code <= 802: # clear
-            return [243, 179, 67]
-        elif 803 <= code and code <= 899: # clouds
-            return [139, 145, 158]
-        else:
-            return [
-                random.randint(64, 255),
-                random.randint(64, 255),
-                random.randint(64, 255)
-        ]
-
     def draw_text(self, x, y, text, color):
         graphics.DrawText(
             self.offscreen_canvas,

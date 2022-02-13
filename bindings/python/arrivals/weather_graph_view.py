4d8d989c43409024cb289e537af052b142d0f805
Dan Choi <ygdanchoi@gmail.com>
2022-02-12 20:16:54 -0500

    nice try

--- bindings/python/arrivals/weather_graph_view.py
+++ bindings/python/arrivals/weather_graph_view.py
@@ -74,7 +74,7 @@ class WeatherGraphViewModel(Observable):
 
             if update_weather_timer == 0:
                 forecast = self.weather_service.get_forecast()
-                self.points = self.create_weather_points(forecast)
+                self.weather_points = self.create_weather_points(forecast)
                 update_weather_timer = 4 * 60
 
             update_weather_timer -= 1

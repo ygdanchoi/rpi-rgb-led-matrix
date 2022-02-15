d6e9e6041c25ce46fe9a8037c5daf2ddf656e8a3
Dan Choi <ygdanchoi@gmail.com>
2022-02-14 22:33:47 -0500

    print math

--- bindings/python/arrivals/weather_graph_view.py
+++ bindings/python/arrivals/weather_graph_view.py
@@ -253,6 +253,7 @@ class WeatherGraphView(Observer, SampleBase):
                         self.draw_stripe_pixel(x, yy, point.color)
 
                         for sunrise_ts in self.viewmodel.sunrise_sunset.sunrises:
+                            print(point.x, math.floor(sunrise_ts * (point.x - points[i + 1].x) / (point.ts - points[i + 1].ts)))
                             if point.x == math.floor(sunrise_ts * (point.x - points[i + 1].x) / (point.ts - points[i + 1].ts)):
                                 self.offscreen_canvas.SetPixel(
                                     x,

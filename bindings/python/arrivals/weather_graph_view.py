040f503c35ffc83b93cf4dd2a0cc071327fa79ce
Dan Choi <ygdanchoi@gmail.com>
2022-02-15 19:43:53 -0500

    add back chevrons

--- bindings/python/arrivals/weather_graph_view.py
+++ bindings/python/arrivals/weather_graph_view.py
@@ -63,9 +63,7 @@ class WeatherGraphViewModel(Observable):
             
             last_delta_s = (time.time_ns() - last_ns) / 1_000_000_000
             s_to_wait = max(0, 0.060 - last_delta_s)
-            print(last_delta_s)
-            await asyncio.sleep(0)
-            # await asyncio.sleep(s_to_wait)
+            await asyncio.sleep(s_to_wait)
             last_ns = time.time_ns()
 
     async def background_thread(self):
@@ -260,23 +258,23 @@ class WeatherGraphView(Observer, SampleBase):
                     for yy in range(y + 1, self.offscreen_canvas.height):
                         self.draw_stripe_pixel(x, yy, point.color)
 
-                        # if point.ts == points[i + 1].ts:
-                        #     continue
+                        if point.ts == points[i + 1].ts:
+                            continue
 
-                        # for sunrise_ts in self.viewmodel.sunrise_sunset.sunrises:
-                        #     mm = (point.x - points[i + 1].x) / (point.ts - points[i + 1].ts)
-                        #     bb = point.x - mm * point.ts
-                        #     xx = math.floor(mm * sunrise_ts + bb)
+                        for sunrise_ts in self.viewmodel.sunrise_sunset.sunrises:
+                            mm = (point.x - points[i + 1].x) / (point.ts - points[i + 1].ts)
+                            bb = point.x - mm * point.ts
+                            xx = math.floor(mm * sunrise_ts + bb)
                             
-                        #     if x == xx and (yy + self.viewmodel.vertical_offset // 4) % 4 == point.y % 4:
-                        #         chevrons_up.append((x, yy, color))
-                        # for sunset_ts in self.viewmodel.sunrise_sunset.sunsets:
-                        #     mm = (point.x - points[i + 1].x) / (point.ts - points[i + 1].ts)
-                        #     bb = point.x - mm * point.ts
-                        #     xx = math.floor(mm * sunset_ts + bb)
+                            if x == xx and (yy + self.viewmodel.vertical_offset // 4) % 4 == point.y % 4:
+                                chevrons_up.append((x, yy, color))
+                        for sunset_ts in self.viewmodel.sunrise_sunset.sunsets:
+                            mm = (point.x - points[i + 1].x) / (point.ts - points[i + 1].ts)
+                            bb = point.x - mm * point.ts
+                            xx = math.floor(mm * sunset_ts + bb)
                             
-                        #     if x == xx and (yy - self.viewmodel.vertical_offset // 4) % 4 == point.y % 4:
-                        #         chevrons_down.append((x, yy, color))
+                            if x == xx and (yy - self.viewmodel.vertical_offset // 4) % 4 == point.y % 4:
+                                chevrons_down.append((x, yy, color))
         
         for point in chevrons_up:
             x = point[0]

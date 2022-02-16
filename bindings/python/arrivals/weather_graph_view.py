78b0e7c3ff9eee05b8d120dad26e459adda0016b
Dan Choi <ygdanchoi@gmail.com>
2022-02-15 20:29:18 -0500

    dammit lol

--- bindings/python/arrivals/weather_graph_view.py
+++ bindings/python/arrivals/weather_graph_view.py
@@ -65,7 +65,6 @@ class WeatherGraphViewModel(Observable):
             
             last_delta_s = (time.time_ns() - last_ns) / 1_000_000_000
             s_to_wait = max(0, 0.055 - last_delta_s)
-            print(last_delta_s)
             await asyncio.sleep(s_to_wait)
             last_ns = time.time_ns()
 

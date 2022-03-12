54821503f8e8a6b9762268eb78b2673592766715
Dan Choi <ygdanchoi@gmail.com>
2022-03-12 07:23:57 -0500

    up

--- arrivals/weather_point_factory.py
+++ arrivals/weather_point_factory.py
@@ -65,7 +65,7 @@ class WeatherPointFactory:
         elif 500 <= code and code <= 599: # rain
             return [81, 121, 243]
         elif 600 <= code and code <= 699: # snow
-            return [230, 229, 228]
+            return [233, 232, 231]
         elif 700 <= code and code <= 799: # fog
             return [50, 182, 122]
         elif 800 <= code and code <= 802: # clear

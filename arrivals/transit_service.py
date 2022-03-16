2051288b823546181a34048f5b0d33eb064281fb
Dan Choi <ygdanchoi@gmail.com>
2022-03-16 14:11:21 -0400

    use process pool executor

--- arrivals/transit_service.py
+++ arrivals/transit_service.py
@@ -306,7 +306,7 @@ class CompositeTransitService(BaseTransitService):
         transit_lines = []
 
         try:
-            with concurrent.futures.ThreadPoolExecutor(max_workers=1) as executor:
+            with concurrent.futures.ProcessPoolExecutor() as executor:
                 futures = [
                     self.loop.run_in_executor(
                         executor, 

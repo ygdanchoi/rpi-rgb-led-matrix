38626e931a3825da1f8b7c79cbac10ef69c08878
Dan Choi <ygdanchoi@gmail.com>
2022-08-07 14:57:30 -0400

    hm

--- arrivals/weather_graph_view.py
+++ arrivals/weather_graph_view.py
@@ -240,12 +240,13 @@ class WeatherGraphView(Observer, SampleBase):
                 )
             else:
                 max_uv = max([pt.uv for pt in points[(p_i - 1):(p_i + 3)]])
-                self.draw_text(
-                    7 + i * 19 - len(max_uv) * self.viewmodel.cell_width / 2,
-                    self.offscreen_canvas.height - 1 - self.viewmodel.cell_height,
-                    max_uv,
-                    [255, 0, 0] if max_uv >= 'uv5' else [255, 0, 255]
-                )
+                if max_uv > 'uv0':
+                    self.draw_text(
+                        7 + i * 19 - len(max_uv) * self.viewmodel.cell_width / 2,
+                        self.offscreen_canvas.height - 1 - self.viewmodel.cell_height,
+                        max_uv,
+                        [255, 0, 0] if max_uv >= 'uv5' else [255, 0, 255]
+                    )
 
         self.offscreen_canvas = self.matrix.SwapOnVSync(self.offscreen_canvas)
 

32530fcebec355f8dd8f37bbd28f5fa810a558fa
Dan Choi <ygdanchoi@gmail.com>
2023-03-31 10:20:25 -0400

    more threads

--- arrivals/transit_feed_view.py
+++ arrivals/transit_feed_view.py
@@ -67,6 +67,7 @@ class TransitFeedViewModel(Observable):
             self.increment_offsets()
             
             last_delta_s = (time.time_ns() - last_ns) / 1_000_000_000
+            print(last_delta_s)
             s_to_wait = max(0, 0.065 - last_delta_s)
             await asyncio.sleep(s_to_wait)
             last_ns = time.time_ns()

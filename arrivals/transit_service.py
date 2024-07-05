fc6d1bf78cc0ec63eb6f730801a7594c4b3ad9af
Dan Choi <ygdanchoi@gmail.com>
2024-07-04 20:53:33 -0400

    filter

--- arrivals/transit_service.py
+++ arrivals/transit_service.py
@@ -126,7 +126,8 @@ class MtaSubwayService(GtfsService):
         return row[1]
 
     def get_trip_id(self, row):
-        print(row[1])
+        if '130300_Q..S' in row[1]:
+            print(row[1])
         match = re.search(r'\d{6}_\w+\.{2}[NS]', row[1])
         return match.group() if match else '' 
     

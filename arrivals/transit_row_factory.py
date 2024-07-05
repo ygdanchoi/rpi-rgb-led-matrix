5ba4f62d51b668860e734616865103e1695bdf8e
Dan Choi <ygdanchoi@gmail.com>
2024-07-04 21:30:28 -0400

    go negative

--- arrivals/transit_row_factory.py
+++ arrivals/transit_row_factory.py
@@ -38,7 +38,7 @@ class TransitRowFactory:
                 color=transit_line.color,
                 y = y,
                 dx_name = -self.beveled_zigzag(
-                    max_rows * cell_height + 3 - pseudo_y,
+                    max_rows * cell_height - 4 - pseudo_y,
                     (len(transit_line.name) - 4) * cell_width,
                     2 * cell_height
                 ) if should_scroll_name else 0,

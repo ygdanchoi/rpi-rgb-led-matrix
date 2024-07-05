d5767e99cc2d17505f2dbff12d7b51feaf96d565
Dan Choi <ygdanchoi@gmail.com>
2024-07-04 21:38:23 -0400

    another pixel

--- arrivals/transit_row_factory.py
+++ arrivals/transit_row_factory.py
@@ -38,14 +38,14 @@ class TransitRowFactory:
                 color=transit_line.color,
                 y = y,
                 dx_name = -self.beveled_zigzag(
-                    max_rows * cell_height + 9 - pseudo_y,
+                    max_rows * cell_height + 12 - pseudo_y,
                     (len(transit_line.name) - 4) * cell_width,
-                    2 * cell_height + 1
+                    2 * cell_height + 2
                 ) if should_scroll_name else 0,
                 dx_description = -self.beveled_zigzag(
-                    max_rows * cell_height + 9 - pseudo_y,
+                    max_rows * cell_height + 12 - pseudo_y,
                     (len(transit_line.description) - 17) * cell_width,
-                    2 * cell_height + 1
+                    2 * cell_height + 2
                 ) if should_scroll_description else 0
             ))
 
